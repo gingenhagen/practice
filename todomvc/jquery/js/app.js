f9f59499b26610a6f093a69438dc45e134acf7b0
Jonathan Chu <jmchuster@gmail.com>
2016-01-20 17:45:15 -0800

    toggle footer

--- todomvc/jquery/js/app.js
+++ todomvc/jquery/js/app.js
@@ -11,16 +11,43 @@ $(function() {
       value: $(this).val()
     });
     $('.items').append(itemHTML);
+    updateDisplay();
 
     $(this).val('');
   });
 
   $('.items').on('click', '.item .toggle', function() {
     if ($(this).prop('checked') === true) {
-      $(this).closest('.item').addClass('completed');
+      $(this).closest('.item').addClass('completed').removeClass('todo');
     } else {
-      $(this).closest('.item').removeClass('completed');
+      $(this).closest('.item').removeClass('completed').addClass('todo');
     }
+    updateCompleted();
   });
+
+  function countCompleted() {
+    return $('.item.todo').length;
+  }
+
+  function updateCompleted() {
+    $('.items-left .count').text(countCompleted());
+  }
+
+  function countItems() {
+    return $('.item').length;
+  }
+
+  function updateFooter() {
+    if (countItems() > 0) {
+      $('.footer').removeClass('hidden');
+    } else {
+      $('.footer').addClass('hidden');
+    }
+  }
+
+  function updateDisplay() {
+    updateCompleted();
+    updateFooter();
+  }
 
 });
