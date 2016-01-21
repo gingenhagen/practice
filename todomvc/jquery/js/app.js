7828c8aff7a7d9369a4b01eeebe4804c4dd42e4f
Jonathan Chu <jmchuster@gmail.com>
2016-01-20 17:35:27 -0800

    we can add items and mark them as completed

--- todomvc/jquery/js/app.js
+++ todomvc/jquery/js/app.js
@@ -1,5 +1,26 @@
 $(function() {
 
-  $('.app').hide().fadeIn();
+  $('#app').hide().fadeIn();
+
+  var itemTemplate = Handlebars.compile($("#item-template").html());
+
+  $('#input').on('keyup', function(event) {
+    if (event.which !== 13) { return; }
+
+    var itemHTML = itemTemplate({
+      value: $(this).val()
+    });
+    $('.items').append(itemHTML);
+
+    $(this).val('');
+  });
+
+  $('.items').on('click', '.item .toggle', function() {
+    if ($(this).prop('checked') === true) {
+      $(this).closest('.item').addClass('completed');
+    } else {
+      $(this).closest('.item').removeClass('completed');
+    }
+  });
 
 });
