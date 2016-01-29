e9c71b1cc31b17e42352abdd1d6b611ef11fe8b0
Jonathan Chu <jmchuster@gmail.com>
2016-01-29 01:23:37 -0800

    clean up using macros

--- todomvc/reagent_todomvc/src/cljs/reagent_todomvc/todo/items.cljs
+++ todomvc/reagent_todomvc/src/cljs/reagent_todomvc/todo/items.cljs
@@ -1,43 +1,37 @@
 (ns reagent-todomvc.todo.items
   (:require [reagent.core :as r]
             [reagent-todomvc.todo.model :as model]
-            [reagent-todomvc.helpers.event-helper :as e]))
+            [reagent-todomvc.helpers.event-helper :as eh]
+            [reagent-todomvc.helpers.reagent-helper :as rh :include-macros true]))
 
 (defn item [item]
   (let [val (r/atom (:text item))
         editing (r/atom false)]
-   [^{:key (:id item)
-      :component-did-update
-        #(.log js/console "item-component-did-update")
-       :component-did-mount
-        #(.log js/console "item-component-did-mount")}
     (fn [item]
-      (letfn [(on-change-completed [e]
-                (model/update-item!
-                  (:id item)
-                  {:completed (e/checked e)})
-                (.log js/console "on-change-completed"))
-              (on-change-text [e]
-                (reset! val (e/value e))
-                (.log js/console "on-change-text"))
-              (on-key-up-text [e]
-                (case (.-key e)
-                  "Enter" (save-text (e/value e))
-                  "Escape" (cancel-text)
-                  :default)
-                (.log js/console "on-key-up-text"))
-              (on-double-click []
-                (if-not @editing (reset! editing true))
-                (.log js/console "on-double-click"))
-              (save-text [text]
-                (model/update-item! (:id item)
-                  {:text text})
-                (reset! editing false)
-                (.log js/console "save-text"))
-              (cancel-text []
-                (reset! val (:text item))
-                (reset! editing false)
-                (.log js/console "cancel-text"))]
+      (letfn
+        [(on-change-completed [e]
+           (model/update-item! (:id item) {:completed (eh/checked e)})
+           (.log js/console "on-change-completed"))
+         (on-change-text [e]
+           (reset! val (eh/value e))
+           (.log js/console "on-change-text"))
+         (on-key-up-text [e]
+           (case (.-key e)
+             "Enter" (save-text (eh/value e))
+             "Escape" (cancel-text)
+             :default)
+           (.log js/console "on-key-up-text"))
+         (on-double-click []
+           (if-not @editing (reset! editing true))
+           (.log js/console "on-double-click"))
+         (save-text [text]
+           (model/update-item! (:id item) {:text text})
+           (reset! editing false)
+           (.log js/console "save-text"))
+         (cancel-text []
+           (reset! val (:text item))
+           (reset! editing false)
+           (.log js/console "cancel-text"))]
         [:li.item {:class (if (:completed item) "completed")}
           [:input {:type "checkbox", :checked (:completed item)
                    :on-change on-change-completed}]
@@ -45,24 +39,21 @@
                       :class (if @editing "editing")}
             [:span.readonly (:text item)]
             [^{:component-did-update
-                 (fn [e]
-                   (.log js/console "input-component-did-update"))
+                #(.log js/console "input-component-did-update")
                :component-did-mount
-                 (fn [e]
-                   (.focus (r/dom-node e))
-                   (.log js/console "input-component-did-mount"))
-               :something "else"
-               :key "test"}
+                (fn [e]
+                  (.focus (r/dom-node e))
+                  (.log js/console "input-component-did-mount"))}
              (fn [] [:input.editable {:type "text", :value @val
-                                         :on-change on-change-text
-                                         :on-key-up on-key-up-text}])]]
+                                      :on-change on-change-text
+                                      :on-key-up on-key-up-text}])]]
           [:button {:type "button",
                     :on-click #(model/remove-item! (:id item))}
-                   "X"]]))
-    item]))
+                   "X"]]))))
 
 (defn items []
-  [:ul (map item
+  ;[:ul (map (fn [x] ^{:key (:id x)} [item x])
+  [:ul (rh/map-keyed item :id
          (case (model/get-filter)
            :all (model/all)
            :active (model/active)
